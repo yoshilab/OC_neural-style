83aa7ca0db2f4ada60d67c2d42785e7bc0a927ae
Tsutsui Yasuyuki <Tsutsui@Tsutsui-no-iMac.local>
2019-05-30 18:08:24 +0900

    totyu redirectがうまくできない

--- sample/sample.py
+++ sample/sample.py
@@ -13,8 +13,8 @@ import math
 from argparse import ArgumentParser
 
 from PIL import Image
-from flask import Flask, Response, request, stream_with_context
-import base64, requests
+from flask import Flask, request, render_template
+import base64, datetime
 
 app = Flask(__name__)
 
@@ -29,17 +29,27 @@ def Base64ToNdarry(img64):
     src = cv2.imdecode(img_np, cv2.IMREAD_ANYCOLOR)
     return src
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/picass', methods=['POST'])
 def sample():
     content_base64 = request.form['image']
-    print(content_base64)
     content = Base64ToNdarry(content_base64)
+
+    dt = datetime.datetime.today()
+    dt_formatted = dt.strftime("%Y%m%d%H%M%S")
+    extension = '.png'
+    capture_img_name = os.path.join(app.config['./images'], 'cap_' + dt_formatted + extension)
+    generate_img_name = os.path.join(app.config['./output'], 'gen_' + dt_formatted + extension)
+
+    cv2.imwrite(capture_img_name, content)
+
     content_g = cv2.cvtColor(content, cv2.COLOR_BGR2GRAY)
-    #cv2.imwrite('./output/' + content_name, content_g)
-    #with open(SAVE_DIR + content_name, "rb") as f:
-    #    rimg = f.read()
-    #rimg64 = base64.b64encode(rimg).decode("UTF-8")
-    return NdarrayToBase64(content_g)
+    cv2.imwrite(generate_img_name, content_g)
+
+    return render_template('index.html', generate_img = generate_img_name, capture_img = capture_img_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
